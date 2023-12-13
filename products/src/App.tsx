f8c2b1be5f2d73c1d81903091e63c97880dc930b
Luka Pitskhelauri <luka.pitsk@gmail.com>
2023-12-13 15:59:53 +0400

    add NavLink to header

--- products/src/App.tsx
+++ products/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from './layout';
 import Home from './components/home';
 import About from './components/about';
+import Contact from './components/contacts';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- products/src/components/header/index.tsx
+++ products/src/components/header/index.tsx
@@ -3,18 +3,23 @@ import { NavLink } from "react-router-dom"
 
 const Header = () => {
 
+    const activeStyle = {
+        fontWeight: "bold",
+        background: 'red'
+    }
+
     return (
         <Nav>
             <div>
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Orange_logo.svg/240px-Orange_logo.svg.png" alt="logo" />
             </div>
             <div>
-                <NavLink to=".">Home</NavLink>
-                <NavLink to="about">About Us</NavLink>
-                <NavLink to="contact">Contact</NavLink>
+                <NavLink style={({ isActive }) => (isActive ? { background: 'red' } : undefined)} end to=".">Home</NavLink>
+                <NavLink style={({ isActive }) => (isActive ? { background: 'red' } : undefined)} to="about">About Us</NavLink>
+                <NavLink style={({ isActive }) => (isActive ? { background: 'red' } : undefined)} to="contact">Contact</NavLink>
             </div>
 
-        </Nav>
+        </Nav >
     )
 }
 
